dd9dcd779e7833df88e08ac70d65e6915167fb97
Ivan Correa <99081193+IvanMCorrea@users.noreply.github.com>
2022-12-05 19:49:36 -0300

    Add array map to display tasks

--- components/ToDoTask.jsx
+++ components/ToDoTask.jsx
@@ -1,13 +1,29 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import React from "react";
 import { containerStyles } from "../styles/styles";
 
 const ToDoTask = () => {
+  const tasks = [
+    { title: "My Task", description: "description" },
+    { title: "My Task", description: "description" },
+    { title: "My Task", description: "description" },
+    { title: "My Task", description: "description" },
+    { title: "My Task", description: "description" },
+  ];
   return (
     <View style={containerStyles.componentContainer}>
-      <TouchableOpacity style={containerStyles.task}>
-        <Text>ToDoTaskTitle</Text>
-      </TouchableOpacity>
+      {tasks
+        ? tasks.map((task, index) => (
+            <TouchableOpacity
+              style={[containerStyles.task, { marginBottom: 20 }]}
+              key={index}
+            >
+              <Text>
+                {task.title}: {task.description}
+              </Text>
+            </TouchableOpacity>
+          ))
+        : null}
     </View>
   );
 };
